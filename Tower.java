c4de799446eeab3ddc24a1a9eec80d3f9a5326eb
isaacgrinker12-dev <isaac.grinker12@gmail.com>
2026-05-05 14:07:29 -0700

    Refactor Tower class attributes and update method
    Updated tower attributes and cooldown logic for different tower types.

--- Tower.java
+++ Tower.java
@@ -1,25 +1,30 @@
 package main;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class Tower {
     private int damage;
     private int cost;
     private int range;
     private String name;
-    private long cooldown;
+    private int cooldown;
     private int lastDamageTime = 0;
     private int x, y;
+    private int timer = 0;
     public Tower (int index, int x, int y){
         this.x = x;
         this.y = y;
         // ADD PRIORITY: TARGETTING THE ENEMY FURTHEST DOWN THE ROAD
         if (index == 1) //Student
         {
-            damage = 50;
+            damage = 100;
             cost = 1;
             range = 3;
-            cooldown = 2000; 
+            cooldown = 120; 
 
         }
         else if (index == 2) //SuperSenior
@@ -27,34 +32,46 @@ public class Tower {
             damage = 3;
             cost = 3;
             range = 1;
-            cooldown = 4000;
+            cooldown = 180;
         }
         else if (index == 3) //Teacher
         {
             damage = 2;
             cost = 2;
             range = 4;
-            cooldown = 2000;
+            cooldown = 160;
         }
         else if (index == 4) //Counselor
         {
             damage = 3;
             cost = 5;
             range = 3;
-            cooldown = 3000;
+            cooldown = 120;
         }
         else if (index == 5) //Principal
         {
             damage = 5;
             cost = 7;
             range = 4;
-            cooldown = 4000;
+            cooldown = 200;
         }
         
     }
-    public void update(){
-        
+    public void update(ArrayList<Enemy> enemies) {
+        timer++;
+        if(timer>=cooldown){
+            timer=0;
+            for(Enemy enemy : enemies) {
+                double distance = Math.sqrt(Math.pow(enemy.x - x, 2) + Math.pow(enemy.y - y, 2));
+                if(distance <= range*32) {
+                    enemy.health-=damage;
+                    break; //REMOVE IF WE WANT SPLASH DAMAGE
+                }
+            }
+        }
     }
+        
+    
     public void draw(Graphics g) {
         g.setColor(Color.BLUE);
         g.fillRect(x, y, 24, 24);
